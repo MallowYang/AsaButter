a38fd751f6b3d388b6fc4dce3e794ef5906ce917
Mallowjz <mallowjz@outlook.com>
2022-10-11 22:17:59 +0800

    Update main2.c # Conflicts: #	main2.c

--- main2.c
+++ main2.c
@@ -4,7 +4,11 @@ int main(int argc, char **argv)
 {
 char first[255], last[255];
 askname(first, last);
+<<<<<<< HEAD
  printf("Hello, %s %s!\n", first, last);
 
+=======
+ printf("Hello and bye, %s %s!\n", first, last);
+>>>>>>> 5bcea26 (Update main2.c)
 return 0;
 }
